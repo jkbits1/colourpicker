c36983a8c11cb57beee52afecd5d8672c2d61203
jkbits1 <jkbits1@users.noreply.github.com>
2014-05-11 02:00:42 +0100

    set preview colour on key events

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 /* Exercise 2: Color picker */
+
+function setPreviewColor(color) {
+
+    if (color === undefined || color === null){
+
+        return;
+    }
+
+    // set bkg color
+    $(".preview").css("background-color", color);
+}
+
+$(document).ready(function(){
+
+    var colour = $(".preview").css("background-color");
+
+    $(document).on('keypress keydown keyup', '#color', function(){
+
+//        alert("hi");
+
+        setPreviewColor($('input').val());
+
+    });
+});
